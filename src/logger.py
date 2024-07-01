3d77ed931ad7355ea745e50cab5d1e6e429c7768
Enoch Adetunji <adetunjienocht@gmail.com>
2024-07-01 21:09:26 +0100

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,17 +2,20 @@ import logging
 import os
 from datetime import datetime
 
+# Create logs directory if it doesn't exist
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Log file name with timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
-
+# Logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(messages)s",
-    level=logging.INFO,
-
-
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 
+
+#logging.info('This is a log message')
