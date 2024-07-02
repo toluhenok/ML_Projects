c76f8177296286ae996e66768ea8980eeb47196c
Enoch Adetunji <adetunjienocht@gmail.com>
2024-07-02 20:07:08 +0100

    Data Transformation update

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -17,7 +17,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -25,7 +25,7 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
